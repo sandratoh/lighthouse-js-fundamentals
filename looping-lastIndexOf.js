78436f8b4e2ee43c52dedf484a5bcfd628989c87
Sandra To <sandra.to.e@gmail.com>
2021-01-17 17:55:36 +0000

    Updated long and short answer

--- looping-lastIndexOf.js
+++ looping-lastIndexOf.js
@@ -21,7 +21,7 @@ console.log(lastIndexOf([], 3), "=?", -1);
 */
 
 
-//WORK ON THIS AGAIN
+//RIGHT ANSWER LONG:
 function lastIndexOf(arr, value) {
   if (arr.length === 0) {
     return -1;
@@ -33,9 +33,10 @@ function lastIndexOf(arr, value) {
       return -1;
     }
   }
+}
 
 
-//RIGHT ANSWER;
+//RIGHT ANSWER SHORT:
 
 function lastIndexOf(arr, val) {
   if (arr.length === 0) {
@@ -50,6 +51,7 @@ function lastIndexOf(arr, val) {
   // as the loop has ended without returning anything.
   return -1;
 }
+
 
 
 console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
